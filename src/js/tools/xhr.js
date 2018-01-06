41ec6b758ee42b1fbd254bdadeb5197384040d7d
Dmitriy Kononov <kondor_06@mail.ru>
2018-01-06 21:48:02 +0300

    add default xhr.js

--- src/js/tools/xhr.js
+++ src/js/tools/xhr.js
@@ -1,64 +1,67 @@
-import {xhrSettings} from './settings.js';
-export function setXhrRequest(requestParameters) {
+export default {
 
-  const ErrorAttr = {
-    FILE: 'xhr.js',
-    MESSADGE: {
-      JSON_ERR: 'XHR: JSON error converting response.',
-      LOAD_ERR: 'Load Error.',
-      CONNECT_ERR: 'Connection error.',
-      TIMEOUT_ERR: 'Сonnection timeout exceeded'
-    }
-  };
+  setRequest(requestParameters) {
 
-  let getError = function (messadge, row, error) {
-    let newError = new SyntaxError(messadge, ErrorAttr.FILE, row);
-    newError.cause = error;
-    return newError;
-  };
-
-  let xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', function () {
-
-    if (xhr.status === 200) {
-      let response = '';
-
-      try {
-        console.log(xhr.response);
-        response = JSON.parse(xhr.response);
-      } catch (error) {
-        requestParameters.callbackError(getError(ErrorAttr.MESSADGE.JSON_ERR,
-          26, error));
+    const ErrorAttr = {
+      FILE: 'xhr.js',
+      MESSADGE: {
+        JSON_ERR: 'XHR: JSON error converting response.',
+        LOAD_ERR: 'Load Error.',
+        CONNECT_ERR: 'Connection error.',
+        TIMEOUT_ERR: 'Сonnection timeout exceeded'
       }
+    };
 
-      requestParameters.callbackSuccess(response);
-    } else {
+    let getError = function (messadge, row, error) {
+      let newError = new SyntaxError(messadge, ErrorAttr.FILE, row);
+      newError.cause = error;
+      return newError;
+    };
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', function () {
+
+      if (xhr.status === 200) {
+        let response = '';
+
+        try {
+          console.log(xhr.response);
+          response = JSON.parse(xhr.response);
+        } catch (error) {
+          requestParameters.callbackError(getError(ErrorAttr.MESSADGE.JSON_ERR,
+            26, error));
+        }
+
+        requestParameters.callbackSuccess(response);
+      } else {
+        requestParameters.callbackError(
+          getError(`${ErrorAttr.MESSADGE.LOAD_ERR} ${xhr.statusText}`,
+          35, ''));
+      }
+    });
+
+    xhr.addEventListener('error', function () {
       requestParameters.callbackError(
-        getError(`${ErrorAttr.MESSADGE.LOAD_ERR} ${xhr.statusText}`,
-        35, ''));
+        getError(`${ErrorAttr.MESSADGE.CONNECT_ERR} ${xhr.statusText}`,
+        42, ''));
+    });
+
+    xhr.addEventListener('timeout', function () {
+      requestParameters.callbackError(getError(
+        `${ErrorAttr.MESSADGE.CONNECT_ERR} (${xhr.timeout}ms.)`,
+        50, ''));
+    });
+
+    xhr.timeout = window.appSettings.xhrSettings.timeout;
+    xhr.open(requestParameters.metod, window.appSettings.xhrSettings.urlApi + requestParameters.url, true);
+    xhr.setRequestHeader('Content-Type', window.appSettings.xhrSettings.contentType);
+
+    if (requestParameters.metod === 'GET') {
+      requestParameters.data = '';
     }
-  });
 
-  xhr.addEventListener('error', function () {
-    requestParameters.callbackError(
-      getError(`${ErrorAttr.MESSADGE.CONNECT_ERR} ${xhr.statusText}`,
-      42, ''));
-  });
-
-  xhr.addEventListener('timeout', function () {
-    requestParameters.callbackError(getError(
-      `${ErrorAttr.MESSADGE.CONNECT_ERR} (${xhr.timeout}ms.)`,
-      50, ''));
-  });
-
-  xhr.timeout = xhrSettings.timeout;
-  xhr.open(requestParameters.metod, xhrSettings.urlApi + requestParameters.url, true);
-  xhr.setRequestHeader('Content-Type', xhrSettings.contentType);
-
-  if (requestParameters.metod === 'GET') {
-    requestParameters.data = '';
+    xhr.send(requestParameters.data);
   }
 
-  xhr.send(requestParameters.data);
-}
+};
