b857d0bf237be9f1de27a3a600df2000ea512a26
Dmitriy Kononov <kondor_06@mail.ru>
2018-01-20 19:16:09 +0300

    2D11

--- src/js/buttons/reference-points-add.js
+++ src/js/buttons/reference-points-add.js
@@ -22,8 +22,6 @@ const spinner = form.querySelector('#points-add-spinner');
 const buttonSubmit = form.querySelector('#points-add-submit');
 const buttonCancel = form.querySelector('#points-add-cancel');
 
-const stor = dataStorage.data;
-
 const showSpinner = () => {
   spinner.classList.remove('invisible');
   buttonSubmit.disabled = true;
@@ -111,6 +109,7 @@ const validateForm = () => {
 };
 
 const submitForm = () => {
+  const stor = dataStorage.data;
   let postData = `name=${name.value}&token=${stor.token}`;
   let urlApp = appUrl.replace('{{dir}}', stor.directory);
   urlApp = urlApp.replace('{{oper}}', stor.operatorId);
